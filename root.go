482bb66090e87fff13ac6aff3f96729f2996dbf4
Tanmay Das <tanmaymishu@gmail.com>
2024-11-25 01:19:30 +0600

    Bump version

--- root.go
+++ root.go
@@ -15,7 +15,7 @@ var shouldRunInteractively = false
 var rootCmd = &cobra.Command{
 	Use:     "",
 	Short:   fmt.Sprintf("%s", os.Getenv("APP_NAME")),
-	Version: "0.1.10",
+	Version: "0.1.11",
 }
 
 // AddCmd adds a new sub-command to the root command.
