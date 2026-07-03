630bdb9efec7dcad26c5e4027bfbdbfd6ea685c6
Tanmay Mishu <tanmaymishu@gmail.com>
2026-07-04 00:01:47 +0600

    Update api stub for logout method

--- root.go
+++ root.go
@@ -15,7 +15,7 @@ var shouldRunInteractively = false
 var rootCmd = &cobra.Command{
 	Use:     "",
 	Short:   fmt.Sprintf("%s", os.Getenv("APP_NAME")),
-	Version: "0.1.27",
+	Version: "0.1.28",
 }
 
 // AddCmd adds a new sub-command to the root command.
