39603ab08d35128a5120a2a737675b4ed69854fa
Tanmay Mishu <tanmaymishu@gmail.com>
2026-07-06 23:31:16 +0600

    Bump version

--- installer.ps1
+++ installer.ps1
@@ -8,9 +8,9 @@ function Install-Lemmego {
 
     if ($arch -match "64-bit") {
         if ($env:PROCESSOR_ARCHITEW6432 -eq "ARM64") {
-            $downloadUrl = "https://github.com/lemmego/cli/releases/download/v0.1.37/lemmego-v0.1.37-windows-arm64.exe"
+            $downloadUrl = "https://github.com/lemmego/cli/releases/download/v0.1.38/lemmego-v0.1.38-windows-arm64.exe"
         } else {
-            $downloadUrl = "https://github.com/lemmego/cli/releases/download/v0.1.37/lemmego-v0.1.37-windows-amd64.exe"
+            $downloadUrl = "https://github.com/lemmego/cli/releases/download/v0.1.38/lemmego-v0.1.38-windows-amd64.exe"
         }
     } else {
         Write-Host "Unsupported architecture: $arch"
